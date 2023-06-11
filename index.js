3bc70f0dfaea9f230eb63c5b14600e8c9a90756f
JoonasMykkanen <42795051+JoonasMykkanen@users.noreply.github.com>
2023-06-11 15:03:30 +0300

    ex 3.3

--- index.js
+++ index.js
@@ -44,6 +44,16 @@ app.get('/api/info', (req, res) => {
 	res.send(response)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id)
+	const person = persons.find(person => {return person.id === id})
+	if (person) {
+		res.json(person)	
+	} else {
+		res.status(404).end()
+	}
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
